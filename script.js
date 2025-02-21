4f8c34c0d3301bdffa5f7fe0875d5a107924a070
Rohitsharma-ITG <rohitsharma1@itgeeks.com>
2025-02-21 11:04:15 +0530

    remove

--- script.js
+++ script.js
@@ -3,13 +3,3 @@ console.log("ok")
 const a = 1
 const b = 2
 console.log(a + b);
-console.log("ok")
-
-const af = 1
-const bf = 2
-console.log(a + b);
-console.log("ok")
-
-const aff = 1
-const bff= 2
-console.log(a + b);
